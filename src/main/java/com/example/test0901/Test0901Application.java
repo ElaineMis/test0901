f7416c381b5eab18e3e019faa39d0370f4c7333f
ElaineMis <1350396534@qq.com>
2019-07-18 10:17:26 +0800

    init project

---
+++ src/main/java/com/example/test0901/Test0901Application.java
@@ -0,0 +1,13 @@
+package com.example.test0901;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test0901Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test0901Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test0901/Test0901ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.test0901;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Test0901ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
